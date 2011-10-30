f41d5640129e160b6b4c272f1e3015045db6765b
Brandon Tilley <brandon@brandontilley.com>
2011-10-30 04:19:43 -0700

    No longer update battlegroups or create realms

--- app/controllers/battlegroups_controller.rb
+++ app/controllers/battlegroups_controller.rb
@@ -1,6 +1,4 @@
 class BattlegroupsController < ApplicationController
-  before_filter :authenticate_admin, :only => [:update]
-
   def index
     @battlegroups = Battlegroup.all
   end
--- app/controllers/realms_controller.rb
+++ app/controllers/realms_controller.rb
@@ -1,16 +1,6 @@
 class RealmsController < ApplicationController
   before_filter :authenticate_admin
 
-  def create
-    name = params[:realm][:name]
-    name.split(',').each do |n|
-      realm = Realm.new name: n
-      realm.battlegroup_id = params[:realm][:battlegroup]
-      realm.save
-    end
-    redirect_to root_path
-  end
-
   def update
     @realm = Realm.find params[:id]
     @realm.seen = params[:realm][:seen]
