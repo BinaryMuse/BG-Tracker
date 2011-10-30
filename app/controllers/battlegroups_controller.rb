1a16d7aadeb242f6f97517caad62404d78dea4d6
Brandon Tilley <brandon@brandontilley.com>
2011-10-30 03:38:52 -0700

    Remove unused battlegroups#update

--- app/controllers/battlegroups_controller.rb
+++ app/controllers/battlegroups_controller.rb
@@ -4,9 +4,4 @@ class BattlegroupsController < ApplicationController
   def index
     @battlegroups = Battlegroup.order('name ASC')
   end
-
-  def update
-    @battlegroup = Battlegroup.find params[:id]
-    render :text => params.to_json
-  end
 end
