2f0767267c2f268e945f9a6cf2706800afc6f493
Brandon Tilley <brandon@brandontilley.com>
2011-10-30 04:40:17 -0700

    Read secret token from config, or use default in development

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Battlegroups::Application.config.secret_token = '91bd4edd699e7ec685099553ef84b71e3c8655ce97443c1316c56fb32ee544f058592744406d67a4825afc365beeb97af1ba08ac5b44c3efb00ebabafbe03df4'
+Battlegroups::Application.config.secret_token = ENV['SECRET_TOKEN'] || '91bd4edd699e7ec685099553ef84b71e3c8655ce97443c1316c56fb32ee544f058592744406d67a4825afc365beeb97af1ba08ac5b44c3efb00ebabafbe03df4'
