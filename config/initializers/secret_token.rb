8511d68b6ef65271a0f388bb49ea85f40497de82
Brandon Tilley <brandon@brandontilley.com>
2011-10-30 00:14:41 -0700

    Initial commit -- rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlegroups::Application.config.secret_token = '91bd4edd699e7ec685099553ef84b71e3c8655ce97443c1316c56fb32ee544f058592744406d67a4825afc365beeb97af1ba08ac5b44c3efb00ebabafbe03df4'
